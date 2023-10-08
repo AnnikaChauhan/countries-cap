85c6668c98974da43481fe78e14ad80f0d357c86
Annika Chauhan <annika.chauhan@apolitical.co>
2023-10-08 11:06:59 +0100

    adds camera

--- src/pages/country.tsx
+++ src/pages/country.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom"
 import { getCountry } from "../api/countries"
-import { Typography, IconButton } from "@mui/material"
-import { useEffect, useState } from "react"
+import { Typography, IconButton, Button } from "@mui/material"
+import { MutableRefObject, useEffect, useRef, useState } from "react"
 import { styled } from "@mui/material/styles"
 import Paper from "@mui/material/Paper"
 import Grid from "@mui/material/Grid"
@@ -9,6 +9,8 @@ import { useCookies } from "react-cookie"
 import { Share } from "@capacitor/share"
 import IosShareIcon from "@mui/icons-material/IosShare"
 import { Capacitor } from "@capacitor/core"
+import { Camera, CameraResultType } from "@capacitor/camera"
+import ImageListItem from "@mui/material/ImageListItem"
 
 export const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -22,6 +24,8 @@ const Country = () => {
   const { id } = useParams()
   const [country, setCountry] = useState<any>(null)
   const [, setCookie] = useCookies(["last-viewed-country"])
+  const [imageElement, setImageElement] = useState<any>(null)
+  // const imageElement = useRef<HTMLImageElement>(null)
 
   const share = async () => {
     return await Share.share({
@@ -30,6 +34,24 @@ const Country = () => {
       url: `http://localhost:3000/country/${id}`,
       dialogTitle: "Share with buddies",
     })
+  }
+
+  const takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+    })
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    var imageUrl = image.webPath
+
+    // Can be set to the src of an image now
+    // imageElement.src = imageUrl
+    setImageElement(imageUrl)
   }
 
   useEffect(() => {
@@ -91,6 +113,27 @@ const Country = () => {
               <IosShareIcon />
             </IconButton>
           )}
+          <Button
+            size="medium"
+            variant="outlined"
+            style={{ marginTop: "24px" }}
+            onClick={takePicture}
+          >
+            Upload Photo
+          </Button>
+          <ImageListItem>
+            {imageElement && (
+              <div style={{ height: 200, width: 200 }}>
+                <img
+                  src={imageElement}
+                  alt="uploaded image"
+                  loading="lazy"
+                  width="200"
+                  height="200"
+                />
+              </div>
+            )}
+          </ImageListItem>
         </>
       )}
     </div>
